d8a06540442674b7e9da8e4b23ac92cf8cefaef4
Kozachenko O <cheshire1308@gmail.com>
2022-02-14 12:29:13 +0200

    n

--- java2022/lab1/lab1/src/main/java/paint/paint.java
+++ java2022/lab1/lab1/src/main/java/paint/paint.java
@@ -3,14 +3,14 @@ package paint;
 public class paint {
 	private String title;
 	private float price;
-	private int cat;
 	private String type;
-	public paint(String title, float price, int cat, String type) {
+	private int cat;
+	public paint(String title, float price, String type, int cat) {
 		super();
 		this.title = title;
 		this.price = price;
-		this.cat = cat;
 		this.type = type;
+		this.cat = cat;
 	}
 	public String getTitle() {
 		return title;
@@ -24,21 +24,22 @@ public class paint {
 	public void setPrice(float price) {
 		this.price = price;
 	}
-	public int getCat() {
-		return cat;
-	}
-	public void setCat(int cat) {
-		this.cat = cat;
-	}
 	public String getType() {
 		return type;
 	}
 	public void setType(String type) {
 		this.type = type;
 	}
+	public int getCat() {
+		return cat;
+	}
+	public void setCat(int cat) {
+		this.cat = cat;
+	}
 	@Override
 	public String toString() {
-		return "{\"title\": \""+title+"\", \"type\": \""+type+"\", \"price\": "+price+", \"cat\" :"+cat+"}";
+		return "{\"title\": \""+ title + "\", \"price\":" + price + ", \"type\":" + type + ", \"cat\":" + cat+"}";
 	}
+	
 	
 }
--- java2022/lab1/lab1/src/main/java/servlet/servlet1.java
+++ java2022/lab1/lab1/src/main/java/servlet/servlet1.java
@@ -11,28 +11,43 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
- * Servlet implementation class servlet1
+ * Servlet implementation class Servlet1
  */
 @WebServlet("/Servlet1")
-public class servlet1 extends HttpServlet {
+public class Servlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public servlet1() {
+    public Servlet1() {
         super();
+        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.setContentType("application/json");
 		PrintWriter out = response.getWriter();
-		paint paint1 = new paint("White Nights", 650.50f, 1, "watercolor");
-		out.println("["+paint1+"]");
+		paint paints = new paint("White Nights", 650.5f, "watercolor", 1);
+		out.println("["+paints+"]");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 	}
 
 }
